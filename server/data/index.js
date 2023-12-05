afe5b57e1a0b4fce032e20a1d6628de9710911ee
sotov1996 <rusikgambit1990@gmail.com>
2023-12-05 16:18:09 +0300

    pickupPrompts updated

--- server/data/index.js
+++ server/data/index.js
@@ -14,12 +14,19 @@ const mood = [
 ]
 
 const pickupPrompts = [
-    "Imagine you are a dating expert generate romanitic 180 character pick up line text for tinder.",
-    "Imagine you are a dating expert generate 180 character Funny Pickup Line text for tinder.",
-    "Imagine you are a dating expert generate 180 character Cute Pickup Lines  text for tinder.",
-    "Imagine you are a dating expert generate 180 character Cheesy Pickup Lines text for tinder.",
-    "Imagine you are a dating expert generate 180 character Sexy Pickup Lines text for tinder."
+    "romanitic pick-up text, 180 character",
+    "funny pick-up text, 180 character",
+    "cute pick-up text, 180 character",
+    "cheesy pick-up text, 180 character",
+    "sexy pick-up text, 180 character"
 ]
+// const pickupPrompts = [
+//     "Imagine you are a dating expert generate romanitic 180 character pick up line text for tinder.",
+//     "Imagine you are a dating expert generate 180 character Funny Pickup Line text for tinder.",
+//     "Imagine you are a dating expert generate 180 character Cute Pickup Lines  text for tinder.",
+//     "Imagine you are a dating expert generate 180 character Cheesy Pickup Lines text for tinder.",
+//     "Imagine you are a dating expert generate 180 character Sexy Pickup Lines text for tinder."
+// ]
 
 module.exports = {
     mood,
